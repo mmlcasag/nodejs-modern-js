f6cdc106b4d697be51ccca7fbf02021959569a71
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-05 16:20:02 -0300

    C32A489 - More on ES Modules

--- response-handler.js
+++ response-handler.js
@@ -3,16 +3,26 @@
 
 // this is the new syntax
 import fs from 'fs';
+import path, { dirname } from 'path'; // you can import multiple functions inline by using ","
+import { fileURLToPath } from 'url';
+
+// using this new syntax we don't have acess to global files anymore
+// therefore, __dirname does not work anymore
+// so this a way to make __dirname work again
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  fs.readFile('my-page.html', 'utf8', (err, data) => {
-    res.send(data);
-  });
+  // fs.readFile('my-page.html', 'utf8', (err, data) => {
+  //   res.send(data);
+  // });
+  res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
-// this is how we used to do
+// this is the old syntax
 // module.exports = resHandler;
 
-// this works only when there's only one thing to export
-// otherwise you have to put "export" in the front of the function you want to export
+// this is the new syntax
+// but this is used when exporting only one function per file
+// otherwise you have to "add" export in the beginning of the function
 // export default resHandler;
